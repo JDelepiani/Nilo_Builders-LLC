7d188e18f9925b9414573f63a473cb8525363e36
Josmer Delepiani <114266517+JDelepiani@users.noreply.github.com>
2023-08-16 12:15:10 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("btn-menu").addEventListener("click", mostrar_menu);
+document.getElementById("back-menu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementById("nav");
+background_menu = document.getElementById("back-menu");
+
+function mostrar_menu(){
+   nav.style.right = "-20px";
+   background_menu.style.display = "block";
+}
+
+function ocultar_menu(){
+   nav.style.right = "-300px";
+   background_menu.style.display = "none";
+}
